2c1a02d3887a5e40b40e75826449372b20895d20
DDaniel-Dev <106210799+DDaniel-Dev@users.noreply.github.com>
2022-09-18 19:50:18 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,29 +1,6 @@
 let express = require('express');
 let app = express();
 
-// -- #1. Meet the Node console -- //
-// console.log("Hello World")
-
-// -- #2. Start a Working Express Server -- //
-// app.get("/", (req, res) => {
-//   res.send("Hello Express");
-// });
-
-// -- #3. Serve an HTML File -- //
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/views/index.html");
-// });
-
-// -- #4. Serve Static Assets -- //
-// app.use("/public", express.static(__dirname + "/public"));
-
-// -- #5. Serve JSON on a Specific Route -- //
-app.get("/json", (req, res) => {
-  res.json({"message": "Hello json"});
-});
-
-
-
 
 
 

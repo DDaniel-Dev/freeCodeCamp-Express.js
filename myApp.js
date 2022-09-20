ba662ae37e28e95471c6f5a05b7cb80aa7b7ccf2
DDaniel-Dev <106210799+DDaniel-Dev@users.noreply.github.com>
2022-09-19 23:05:34 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -7,7 +7,10 @@ let app = express();
 // -- #1. Meet the Node console -- //
 console.log("Hello World");
 
-
+// -- #2. Start a Working Express Server -- //
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
 
 
